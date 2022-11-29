d96fdb8c8b0eb4d7d079c0dc1076f3bbd87bdd22
AlexGuillemin91 <alex.guillemin@edu.devinci.fr>
2022-11-29 10:09:36 +0100

    leaflet + iframe

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <Tutorial/>
+  <div>
+    <iframe width="600" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+      src="https://www.openstreetmap.org/export/embed.html?bbox=2.225072085857392%2C48.89240562492582%2C2.2280976176261906%2C48.894073854565384&amp;layer=mapnik"
+      style="border: 1px solid black"></iframe>
+  </div>
 </template>
 
 <script>
