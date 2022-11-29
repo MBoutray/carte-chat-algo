244fd609c46207c79e687b475d12f7178aafe577
AlexGuillemin91 <alex.guillemin@edu.devinci.fr>
2022-11-29 10:58:45 +0100

    add map components

---
+++ nuxt/components/Map.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="map-wrap" style="height: 100vh">
+        <client-only>
+            <l-map :zoom=17 :center="[48.89324, 2.22692]">
+                <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+                <l-marker :lat-lng="[48.89324, 2.22692]"></l-marker>
+            </l-map>
+        </client-only>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Map',
+    methods: {
+        getUserLocation() {
+            navigator.geolocation.getCurrentPosition((position) => {
+                return [position.coords.latitude, position.coords.longitude];
+        });
+    }
+    }
+}
+</script>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,16 +1,12 @@
 <template>
-  <div id="map-wrap" style="height: 100vh">
-    <client-only>
-      <l-map :zoom=17 :center="[48.89324, 2.22692]">
-        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-        <l-marker :lat-lng="[48.89324, 2.22692]"></l-marker>
-      </l-map>
-    </client-only>
-  </div>
+  <Map />
 </template>
 
 <script>
+import Map from '../components/Map.vue';
+
 export default {
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { Map }
 }
 </script>
