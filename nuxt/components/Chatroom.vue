0785b044cd117e3d43ccf9d4ca372ec6db18187e
MBoutray <martindeboutray@hotmail.fr>
2022-11-30 10:01:27 +0100

    Created chatroom

---
+++ nuxt/components/Chatroom.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>chatroom</p>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      messages: [
+        {
+          id: 1,
+          content: "message 1",
+        }
+      ]
+    }
+  },
+  props: ['name', 'messages']
+}
+</script>
